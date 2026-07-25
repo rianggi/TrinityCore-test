e9ed849d57b99b272cd945067c3bef894842130d
rianggi <rianggi@hotmail.com>
2026-07-25 16:10:35 +0800

    sql: remove ALTER TABLE updates and non-CN/EN locale files

---
+++ sql/updates/world/master/2026_06_08_04_world_2025_03_08_04_world.sql
@@ -0,0 +1,223 @@
+-- Marshal Dughan - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=240/marshal-dughan
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('240', 'ruRU', 'Ох, тут и так сложно поддерживать порядок, а тут еще и новые неприятности вылезают! Надеюсь, у тебя хорошие новости, $n...', '0');
+
+
+
+-- Goldtooth - Incorrect quest description: now it says that Goldtooth hid in his lair ABOVE the mine, but on the wowhead it says that he hid IN the mine.
+
+-- https://www.wowhead.com/classic/quest=87/goldtooth
+
+
+
+
+-- William Pestle - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=253/william-pestle
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('253', 'ruRU', 'Ага! Добрый день, добрый день, Мастер $C! Присаживайтесь и выпейте. У вас предприимчивый взгляд, и я думаю, вы найдете разговор со мной стоящим вашего времени...', '0');
+
+
+
+-- Further Concerns - Incorrect quest description. Marshal Dughan offers to use his mount, but he doesn’t give you any mount, and the wowhead doesn’t mention a mount.
+
+-- https://www.wowhead.com/classic/quest=35/further-concerns
+
+
+
+
+-- Rema Schneider - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=1428/rema-schneider
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('1428', 'ruRU', 'Приветствую, $n.', '0');
+
+
+
+-- Guard Thomas - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=261/guard-thomas
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('261', 'ruRU', 'Привет, гражданин. Выглядишь как $gмужчина:женщина; с целью - у тебя есть дело к армии Штормграда?', '0');
+
+
+
+-- Surena Caledon - Missing ruRU localisation text
+
+-- https://www.wowhead.com/classic/quest=1688/surena-caledon
+
+
+
+
+-- Grelin Whitebeard - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=786/grelin-whitebeard
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('786', 'ruRU', 'Приветствую, $Gпарень:девчёнка;. Я Грелин Белобород. Я здесь, чтобы изучить угрозу, которую представляет растущее число троллей в Холодной долине. Что я обнаружил? Это немного тревожно...', '0');
+
+
+
+-- Mountaineer Stormpike - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=1343/mountaineer-stormpike
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('1343', 'ruRU', 'Так, молодой, ощетинившийся $c, несомненно, привлечённый сюда разговорами о моих подвигах на полях сражений!$B$BСейчас не время для историй, ибо есть великие, важные дела, которые нужно совершить! Так что если вы ищете славы, то удача вам сегодня улыбнётся...', '0');
+
+
+
+-- Woo Ping <Weapon Master> - Missing title
+
+-- https://www.wowhead.com/classic/npc=11867/woo-ping
+
+
+
+
+-- The Collector - Incorrect quest text
+
+-- https://www.wowhead.com/classic/quest=123/the-collector
+
+
+
+
+-- The Collector's Ring - There is no description of the item
+
+-- https://www.wowhead.com/classic/item=2239/the-collectors-ring
+
+
+
+
+-- Farmer Furlbrow - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=237/farmer-furlbrow
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('237', 'ruRU', 'В этих краях одни неприятности. Я пытался сказать этому дураку Сальдену, чтобы он убирался, пока он ещё может, но он и слышать об этом не хочет. Но я не дурак. Мы с Верной уедем отсюда, как только починим эту повозку.', '0');
+
+
+
+-- Verna Furlbrow - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=238/verna-furlbrow
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('238', 'ruRU', 'Иногда мне кажется, что на небе большая серая туча, которая просто обрушивает на нас несчастья. Сначала нас прогнали с нашей земли, а теперь мы даже не можем выбраться из Западного Края. Кругом бардак. Что-то нужно делать.', '0');
+
+
+
+-- Salma Saldean - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=235/salma-saldean
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('235', 'ruRU', 'Добро пожаловать в нашу скромную обитель! Всегда приятно видеть дружелюбное лицо. И какие у вас сильные руки. Мы с мужем всегда ищем помощь на ферме. Теперь, когда большинство хороших людей уехали, трудно найти способного человека, который мог бы помочь.', '0');
+
+
+
+-- Gryan Stoutmantle - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=234/gryan-stoutmantle
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('234', 'ruRU', 'Западный Край погряз в отвратительной коррупции. Пока я исполнял свой долг на полях сражений Лордерона, эти честные фермы были захвачены и превращены в убежища для головорезов и убийц. Народному ополчению нужна ваша помощь.', '0');
+
+
+
+-- Magistrate Solomon - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=344/magistrate-solomon
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('344', 'ruRU', 'Красногорье охвачено хаосом!', '0');
+
+
+
+-- Kimberly Hiett <Fletcher> - Incorrect title
+
+-- https://www.wowhead.com/classic/npc=789/kimberly-hiett
+
+
+
+
+-- Darcy <Waitress> - Missing title
+
+-- https://www.wowhead.com/classic/npc=379/darcy
+
+
+
+
+-- Turalyon's Monument in Stormwind - Incorrect title of Uter
+
+
+INSERT INTO `page_text_locale` (`ID`, `locale`, `Text`) VALUES
+
+(290, 'ruRU', 'Генерал Туралион\r\n\r\nБывший наместник лорда Андуина Лотара. Рыцарь Серебряной Длани. Верховный генерал экспедиции Альянса, которая отправилась в Дренор, родной мир орков. Предположительно погиб.\r\n\r\nЭзарус тар-но Дарадор – Кровью и честью мы служим.\r\nТы был правой рукой справедливости и добродетели, старый друг. Твое имя всегда будет почитаться в наших залах.\r\n\r\n- Лорд Утер Светоносный, рыцарь Серебряной Длани');
+
+
+
+-- Protector of the People <The People's Militia> - Missing title
+
+-- https://www.wowhead.com/classic/npc=8096/protector-of-the-people
+
+
+
+
+-- Wilder Thistlenettle - Missing greeting
+
+-- https://www.wowhead.com/classic/npc=656/wilder-thistlenettle
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('656', 'ruRU', 'Везде были воры! $b$bЭто было ужасно. Пещера обрушилась на нас. Я думаю, что все шахтёры мертвы, включая моего брата, бригадира.', '0');
+
+
+
+-- Postponing the Inevitable - Archmage Berinand https://wow.66wan.net/wotlk/en/?npc=25314 speaks about himself in feminine gender
+
+-- https://www.wowhead.com/wotlk/quest=11905/postponing-the-inevitable
+
+
+
+
+-- Quickening - Archmage Berinand https://www.wowhead.com/wotlk/npc=25314/archmage-berinand speaks about himself in feminine gender, and incorrect questgiver in objectives
+
+-- https://www.wowhead.com/wotlk/quest=11911/quickening
+
+
+
+
+
+-- Have They No Shame? (Alliance) - Incorrect questgiver in objectives
+
+-- https://www.wowhead.com/wotlk/quest=13094/have-they-no-shame
+
+
+
+
+-- Have They No Shame? (Horde) - Incorrect questgiver in objectives
+
+-- https://www.wowhead.com/wotlk/quest=13095/have-they-no-shame
+
+
+
+
+-- Count Remington Ridgewell <House of Nobles> - Missing greeting
+
+-- https://www.wowhead.com/wotlk/npc=2285/count-remington-ridgewell
+
+
+INSERT INTO quest_greeting_locale (ID, locale, Greeting, VerifiedBuild) VALUES ('2285', 'ruRU', 'Моя семья может похвастаться лучшей коллекцией ювелирных изделий и предметов изящного искусства среди всех дворян Штормграда!$b$bИ мы всегда хотим увеличить размер нашей коллекции...', '0');
+
+
+
+-- Collection of Goods - Just a quick clarification that it's not "Stratholme Water" but "Water FROM Stratholme"
+
+-- https://www.wowhead.com/wotlk/quest=7642/collection-of-goods
+
+
